baba973718aeb228355f6dc478023c2d4bb8fb89
Ziyang Wen <wenziyang007@gmail.com>
2024-09-04 13:33:34 +0800

    Set up Basic Routers and Expected Pages

--- frontend-management/src/App.jsx
+++ frontend-management/src/App.jsx
@@ -1,8 +1,35 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import UserDetail from "./pages/UserDetail";
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
-    </div>
+    <Router>
+      <div>
+        {/* 导航链接 */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/about">Users</Link>
+            </li>
+            <li>
+              <Link to="/userDetail">UserDetail</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* 定义路由 */}
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/about" element={<Users />} />
+          <Route path="/userDetail" element={<UserDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
