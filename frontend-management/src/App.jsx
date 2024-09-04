d5468cddfe544053a073965a11486855181c0fb9
Ziyang Wen <wenziyang007@gmail.com>
2024-09-04 12:37:56 +0800

    Install Tailwindcss and Set up File Structures

--- frontend-management/src/App.jsx
+++ frontend-management/src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>1</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
+  );
 }
 
 export default App;
