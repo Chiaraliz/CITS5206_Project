f9f3ddd1451bfbc33297a2a514246f83d7da4e23
Ziyang Wen <wenziyang007@gmail.com>
2024-09-05 15:56:54 +0800

    Update Header and Dashboard

--- frontend-management/src/components/Header.jsx
+++ frontend-management/src/components/Header.jsx
@@ -1,13 +1,25 @@
 import styled from "styled-components";
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar } from "antd";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 function Header() {
-  return <StyledHeader>HEADER</StyledHeader>;
+  return (
+    <StyledHeader>
+      <Avatar size="large" icon={<UserOutlined />} />
+      <div>Username</div>
+    </StyledHeader>
+  );
 }
 
 export default Header;
