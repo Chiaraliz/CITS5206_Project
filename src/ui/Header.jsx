181295c45acc87f350724586dc26a2789d2ea74f
Chiaraliz <15962100649@163.com>
2024-09-05 22:21:44 +0800

    complete header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,21 @@
+import Box from "./Box";
+import Button from "./Button";
+import WelcomeUser from "../features/profile/WelcomeUser";
 function Header() {
-  return <div>header</div>;
+  return (
+    <div className="flex justify-between px-32 py-5 border-b shadow-sm">
+      <a href="https://aasyp.org/">
+        <img
+          className="h-24"
+          src="/cropped-AASYP-Logo-FC-Transparent-300x170.webp"
+        />
+      </a>
+      <Box>
+        <WelcomeUser />
+        <Button type="logout">Log out</Button>
+      </Box>
+    </div>
+  );
 }
 
 export default Header;
--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -2,7 +2,7 @@ function Logo() {
   return (
     <div className="flex justify-center">
       <img
-        className=" h-32"
+        className="h-32"
         src="/cropped-AASYP-Logo-FC-Transparent-300x170.webp"
       ></img>
     </div>
