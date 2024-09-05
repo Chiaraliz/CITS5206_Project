ca0dbe6f3b9d8de0837e5d4e80514b1c9963ff01
Chiaraliz <15962100649@163.com>
2024-09-05 14:07:18 +0800

    set the login page layout and logo

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <div className="flex h-screen">
+      <div className="flex-1">1</div>
+      <div className="flex-1">2</div>
+    </div>
+  );
+}
+
+export default Login;
