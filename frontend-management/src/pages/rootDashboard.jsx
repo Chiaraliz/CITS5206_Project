69df5077bdb422aa0daf60bf39f4d462bba12ffe
SexyZoe <91857835+SexyZoe@users.noreply.github.com>
2024-09-20 19:42:32 +1000

    Create rootDashboard.jsx

---
+++ frontend-management/src/pages/rootDashboard.jsx
@@ -0,0 +1,27 @@
+// src/pages/RootDashboard.jsx
+import Heading from '../components/Heading';
+import AdminTable from '../components/AdminTable'; // 引入AdminTable
+import AdminTableOperations from '../components/AdminTableOperations'; // 引入AdminTableOperations
+import { Row, Col } from 'antd';
+
+const RootDashboard = () => {
+  return (
+    <>
+      <Row style={{ marginBottom: '24px' }}>
+        <Heading as="h1">Admin Management</Heading>
+      </Row>
+      <Row style={{ marginBottom: '24px' }}>
+        <Col span={24}>
+          <AdminTableOperations />
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24}>
+          <AdminTable />
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+export default RootDashboard;
